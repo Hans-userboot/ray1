ea2038f3c905dabc7a80eb8bd683b3851fb8909e
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2024-08-15 20:26:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ FORCE_MESSAGE = os.environ.get(
 )
 
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5178772086 5575087342 6039932222").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5178772086 5575087342 6039932222 7150384119").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
     
